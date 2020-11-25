cc511dbd725451ac44ae13c064bcffd1f41556df
sumant mishra <sumantmishra511@gmail.com>
2020-11-25 23:17:20 +0530

    About content added

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { ToggleStatusEmit } from "../header/header.type";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.scss"],
 })
 export class AboutComponent implements OnInit {
+  @Output() toggleStatus = new EventEmitter<ToggleStatusEmit>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  // this function is for scrollPage of pages
+  scrollPage(selectedPage: string) {
+    const toggleDataEmit = {
+      toggleStatus: false,
+      selectedPage: selectedPage,
+    };
+    this.toggleStatus.emit(toggleDataEmit); // this emits the toggle status to parent component so that it can open or close the scrollPage accordingly.
   }
-
 }
