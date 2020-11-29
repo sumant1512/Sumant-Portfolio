1bdb98072ffb78ab367ac9981fd732757d02f1b1
sumant mishra <sumantmishra511@gmail.com>
2020-11-29 13:42:01 +0530

    code clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,8 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { PortfolioComponent } from "./portfolio/portfolio.component";
-import { RecentWorkComponent } from "./recent-work/recent-work.component";
 import { AboutComponent } from "./about/about.component";
 import { ContactComponent } from "./contact/contact.component";
-import { HomeContentComponent } from "./home-content/home-content.component";
 import { LightBottomSvgComponent } from "./light-bottom-svg/light-bottom-svg.component";
 import { DarkBottomSvgComponent } from "./dark-bottom-svg/dark-bottom-svg.component";
 import { FooterComponent } from "./footer/footer.component";
@@ -22,10 +20,8 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
     HeaderComponent,
     HomeComponent,
     PortfolioComponent,
-    RecentWorkComponent,
     AboutComponent,
     ContactComponent,
-    HomeContentComponent,
     LightBottomSvgComponent,
     DarkBottomSvgComponent,
     FooterComponent,
