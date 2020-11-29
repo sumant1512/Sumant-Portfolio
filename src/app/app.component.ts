8bd49d467628ff065ed0b95d233fb4b77f983e8e
sumant mishra <sumantmishra511@gmail.com>
2020-11-29 12:23:04 +0530

    conde clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,21 +23,8 @@ export class AppComponent {
 
   myStyle: object = {};
   myParams: object = {};
-  width: number = 100;
-  height: number = 100;
 
   ngOnInit() {
-    this.myStyle = {
-      position: "fixed",
-      width: "100%",
-      height: "100%",
-      "z-index": -1,
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-    };
-
     this.myParams = {
       particles: {
         number: {
